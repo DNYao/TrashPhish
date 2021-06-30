7dacc511f609741df5cd33062125dab082e883ab
David Yao <davidnyao@gmail.com>
2021-06-30 19:59:51 -0400

    Update TrashPhish.py

--- TrashPhish.py
+++ TrashPhish.py
@@ -10,85 +10,76 @@ import os
 import sys
 import string
 import json
+import random
+
 from threading import Thread
-from random import randint, choice, seed
+from random import randint
 from random_username.generate import generate_username
 from lxml.html import fromstring
 from itertools import cycle
+from utils import passwords, names, header_info
+from time import sleep
 
-url = '#URL HERE#'
-
-chars = string.ascii_letters + string.digits + '!@#$%^&*()'
-seed = (os.urandom(1024))
-names = json.loads(open('names.json').read())
+# Phishing URL here
+url = ''
+# If you need to click on a link to get to the actual fake login
+referrer = None
 
 #Number of cycles for TrashPhish to run against the phishing site
-num = int (sys.argv[1]) 
-
-headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36", 
-    "Accept-Encoding":"gzip, deflate, br", 
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
-    "DNT":"1",
-    "Connection":"close", 
-    "Upgrade-Insecure-Requests":"1"
-    }
-
-def get_proxies():
-    url = 'https://free-proxy-list.net/'
-    response = requests.get(url)
-    parser = fromstring(response.text)
-    proxies = set()
-    for i in parser.xpath('//tbody/tr')[:10]:
-        if i.xpath('.//td[7][contains(text(),"yes")]'):
-            #Grabbing IP and corresponding PORT
-            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
-            proxies.add(proxy)
-    return proxies
-
-proxies = get_proxies()
-proxy_pool = cycle(proxies)
+num = 5
 
 def trash_poster():
     """
     Spits out two flavors of junk to make detection and differentation from legit account info a tiny bit harder
     """
-    #length of password
-    size = 21
 
+    proxydict = [{'http':'213.137.240.243:81', 'http':'103.103.3.6:8080'},
+                {'http':'157.230.103.189:43844', 'http':'187.19.207.195:3128'},
+                {'http':'45.160.224.97:8080','http':'141.94.106.133:8080'},
+                {'http':'121.244.213.162:8080','http':'34.254.255.177:4001'}
+                ]
+
+    print("Posting trash")
+    sleep(randint(1,4))
     while True:
         #If the site takes an email address, otherwise remove the choice(domains) from user generation
         domains = ["@yahoo.com","@hotmail.com","@gmail.com","@aol.com","@mail.com"]
+        variable = randint(0,20)
+        password = passwords[randint(0,10000)]
 
-        if randint(0,2) % 2 == 0:
-            for name in names:
-                name_extra = ''.join(choice(string.digits))
-                username = name.lower() + name_extra + choice(domains)
-                password = id_generator(chars, size)
+        if variable % 2 == 0:
+            username = str(names[randint(0,100)] + str(randint(0,5000)))
+            if len(password) < 6:
+                password += str(randint(0,100))
+
+        elif variable % 3 == 0:
+            username = generate_username(1)[0].lower()
+            if len(password) < 6:
+                password += str(randint(0,100))
+
         else:
-            username = generate_username(1)[0] + choice(domains)
-            password = id_generator(chars, size)
+            username = str(names[randint(0,100)].lower() + str(names[randint(0,100)].lower()) + str(randint(0,5000)))
+            if len(password) < 6:
+                password += str(randint(0,100))
 
         for _ in range(1, 11):
             #Fill in user form and password form with appropriate form data
             try:
-                proxy = next(proxy_pool)
-            except:
-                print("Connection error. Skipping proxy.")
-            try:
+                # comment out if no referrer
+                r = requests.get(referrer)
+                sleep(randint(1,4))
+
+                # Change 'username' and 'password' depending on html
                 r = requests.get(url)
-                requests.post(url, proxies = {"http": proxy, "https": proxy}, headers = headers, allow_redirects=False, data={
-                    '#USER FORM': username,
-                    '#PASSWORD FORM': password
+                requests.post(url, headers = header_info(referrer), proxies=random.choice(proxydict), allow_redirects=False, data={
+                    'username': username,
+                    'password': password
                 })
             except HTTPError:
                 print(HTTPError)
                 break
 
         print('sending username %s and password %s' % (username, password))
-
-def id_generator(chars, size):
-	return ''.join(choice(chars) for i in range(size))
 
 if __name__ == "__main__":
     for k in range(0, num):
